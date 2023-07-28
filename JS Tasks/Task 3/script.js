c6bf4811802ebc0ea445d22bcddaeb42c8ac4fc4
Tomas <RateAHome@outlook.com>
2023-07-28 15:25:32 +0100

    task-3

--- JS Tasks/Task 3/script.js
+++ JS Tasks/Task 3/script.js
@@ -11,4 +11,23 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+document.getElementById("btn").addEventListener("click", function () {
+  fetch(ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+      let output = "";
+      data.forEach((user) => {
+        output += `
+          <div class="card">
+            <img src="${user.avatar_url}" alt="Avatar of ${user.login}">
+            <h2>${user.login}</h2>
+          </div>
+        `;
+      });
+      document.getElementById("output").innerHTML = output;
+    })
+    .catch((error) => console.error(error));
+  document.getElementById("message").style.display = "none";
+});
