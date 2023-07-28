95b054f39754cf4e56d64f79a5e6e15c1d342e4a
Tomas <RateAHome@outlook.com>
2023-07-28 15:19:07 +0100

    task-1

--- JS Tasks/Task 1/script.js
+++ JS Tasks/Task 1/script.js
@@ -8,3 +8,32 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 div elemente su id "output" viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+document.getElementsByTagName("form")[0].addEventListener("submit", convert);
+
+function convert(event) {
+    event.preventDefault();
+
+    let weightInKg = document.getElementById("search").value;
+
+    if (!weightInKg || weightInKg < 0) {
+        alert("Please enter valid weight in kilograms");
+        return;
+    }
+
+    let weightInPounds = weightInKg * 2.2046;
+    let weightInGrams = weightInKg / 0.001;
+    let weightInOunces = weightInKg * 35.274;
+
+    document.getElementById("output").innerHTML = `<div class="result">
+        <div class="number">${weightInPounds.toFixed(2)}</div>
+        <div class="unit">Pounds (lb)</div>
+     </div>
+     <div class="result">
+        <div class="number">${weightInGrams.toFixed(2)}</div>
+        <div class="unit">Grams (g)</div>
+     </div>
+     <div class="result">
+        <div class="number">${weightInOunces.toFixed(2)}</div>
+        <div class="unit">Ounces (oz)</div>
+     </div>`;
+}
